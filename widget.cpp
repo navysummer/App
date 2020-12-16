112ae3f228c2e0e04fbf2a28f1ef4f464a541574
navysummer <navysummer@yeah.net>
2020-12-16 18:07:27 +0800

    add

--- widget.cpp
+++ widget.cpp
@@ -21,36 +21,34 @@ Widget::~Widget()
 
 void Widget::on_searchBtn_clicked()
 {
+    //    qDebug() << QSslSocket::supportsSsl() << QSslSocket::sslLibraryBuildVersionString() << QSslSocket::sslLibraryVersionString();
+    //    qDebug()<<url<<endl;
     QString keyword = ui->KeyWord->text();
-    QString url = "https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=1&tn=baidu&wd=";
+    QString url = "https://www.baidu.com/s?ie=utf-8&wd=";
     url.append(keyword);
-//    qDebug() << QSslSocket::supportsSsl() << QSslSocket::sslLibraryBuildVersionString() << QSslSocket::sslLibraryVersionString();
-//    qDebug()<<url<<endl;
     QNetworkRequest request;
     QNetworkAccessManager* naManager = new QNetworkAccessManager(this);
     QMetaObject::Connection connRet = QObject::connect(naManager,
                                                        SIGNAL(finished(QNetworkReply*)),
                                                        this, SLOT(requestFinished(QNetworkReply*)));
-//    Q_ASSERT(connRet);
 
-//    request.setRawHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36");
-//    request.setRawHeader("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
-//    request.setRawHeader("Host","www.baidu.com");
-//    request.setRawHeader("Connection","keep-alive");
-//    request.setRawHeader("Upgrade-Insecure-Requests","1");
-//    request.setRawHeader("","");
-//    request.setRawHeader("","");
-//    QSslConfiguration config;
-//    config.setPeerVerifyMode(QSslSocket::VerifyNone);
-//    config.setProtocol(QSsl::SslV3);
-//    request.setSslConfiguration(config);
+    request.setRawHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36");
+    request.setRawHeader("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
+    request.setRawHeader("Host","www.baidu.com");
+    request.setRawHeader("Connection","keep-alive");
+    request.setRawHeader("Upgrade-Insecure-Requests","1");
+    QSslConfiguration config;
+    config.setPeerVerifyMode(QSslSocket::VerifyNone);
+    config.setProtocol(QSsl::TlsV1_2);
+    request.setSslConfiguration(config);
     request.setUrl(QUrl(url));
-    naManager->get(request);
+    QNetworkReply *reply = naManager->get(request);
+    reply->ignoreSslErrors();
 
 }
 void Widget::requestFinished(QNetworkReply* reply)
 {
-      qDebug() << QString(reply->readAll()).replace("\n","").replace("\t","");
+      qDebug() << QString(reply->readAll());
       this->hide();
        QString keyword = ui->KeyWord->text();
        emit infoSend(keyword);
